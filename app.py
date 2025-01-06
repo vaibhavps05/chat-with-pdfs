fd66d303e42942ace17585a4a1a7b8b6022ef684
vaibhavps05 <vaibhavps3005@gmail.com>
2025-01-06 23:32:51 +0530

    Gemini llm added

--- app.py
+++ app.py
@@ -4,14 +4,19 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 def get_text_from_pdf(uploaded_files):
+
     text = ""
     for pdf in uploaded_files:
         pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
             text += page.extract_text()
+
     return text
 
 def create_text_chunks(text):
@@ -35,8 +40,20 @@ def create_vectors(text_chunks):
 
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+   
     return vector_store
 
+def create_conversation(vector):
+
+    llm = ChatGoogleGenerativeAI(model="models/llm-001")
+    memory = ConversationBufferMemory(memory_key="pdf_conversation", return_messages=True)
+    conversation = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector.as_retriever(),
+        memory=memory
+    )
+    
+    return conversation
 
 
 
@@ -45,6 +62,9 @@ def main():
     load_dotenv()
     st.set_page_config(page_title="Upload Multiple PDFs and chat with them")
 
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    
     st.header("Chat with PDFs Application")
     st.text_input("Ask anything about your PDFs:")
     
@@ -53,16 +73,18 @@ def main():
 
     if st.button("PROCESS"):
         with st.spinner("Processing..."):
-            #get text
+            # get text
             simple_text = get_text_from_pdf(uploaded_files)
 
-            #create chunks
+            # create chunks
             text_chunks = create_text_chunks(simple_text)
 
-            #create vectors
+            # create vectors
             vector = create_vectors(text_chunks)
-            st.write(vector)
 
+            # convo with vectors
+            st.session_state.conversation = create_conversation(vector)
+    
 
     
 
